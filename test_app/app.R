033b4c256ee39b6ff9f3e61b403c0905a3d9cc15
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2024-02-08 10:58:10 +0000

    finishing touches to app: - added upload - axis and slider rangers generalise across datasets - javascript to update log slider range with updateSliderInput - addded ability to group/colour code scatter by object names - probably other things that I've forgotten

--- test_app/app.R
+++ test_app/app.R
@@ -167,12 +167,33 @@ $(document).ready(function() {
     logifySlider('num_posts', sci = false)
   }, 5)})
 "
+JS.observeSliderChange <-
+  "
+// execute whenever slider input changes
+$(document).on('change', '#data_upload', function() {
+// wait a few ms to allow other scripts to execute
+  setTimeout(function() {
+    // include call for each slider
+    // logifySlider('num_posts', sci = false)
+    logifySlider('num_posts', sci = false);
+}, 5)});
+"
+JS.sendCustomMessage <-
+  "
+$(document).on('shiny:connected', function(event) {
+  Shiny.addCustomMessageHandler('jsCode', function(message) {
+    eval(message.code);
+  });
+});
+"
 # ----
 
 ui <- fluidPage(
   
   tags$head(tags$script(HTML(JS.logify))),
   tags$head(tags$script(HTML(JS.onload))),
+  tags$head(tags$script(HTML(JS.sendCustomMessage))),
+  # tags$head(tags$script(HTML(JS.observeSliderChange))),
   
   # Application title
   titlePanel("Explore Google Search Data"),
@@ -192,11 +213,13 @@ ui <- fluidPage(
                          choices = NULL, selected = NULL, multiple = TRUE),
       shiny::conditionalPanel(condition = "input.chart_type == 1",
                               sliderInput("num_posts", div(HTML("Number of Posts <i>(note log scale)</i>")),
-                                          min = 0.1, max = 5, step = 0.05, value = c(0.1,5), round = TRUE, tick = FALSE),
+                                          min = 0.1, max = 3, step = 0.005, value = c(0.1,3), round = TRUE, tick = FALSE),
                               sliderInput("growth_range", "Growth (%)",
                                           min = -1.5, max = 10, 
                                           value = c(-1.5, 10), 
                                           tick = FALSE)),
+      shiny::selectInput("grouping_variable", "Grouping Variable",
+                         choices = NULL, selected = NULL, multiple = TRUE),
       shiny::selectInput("static_keyword_callout", "Static Chart Callouts",
                          choices = NULL, selected = NULL, multiple = TRUE),
       actionButton("shuffle_label", "Shuffle Label Position")
@@ -205,6 +228,7 @@ ui <- fluidPage(
     # Show a plot of the generated distribution
     mainPanel(
       shiny::tabsetPanel(type = "tabs",
+                         tabPanel("hover chart", dataTableOutput("hover_chart")),
                          tabPanel("Dynamic Chart",
                                   # absolutePanel(top = 10, right = 10, downloadButton("scatter_download")),
                                   plotlyOutput("keyword_dynamic_plot"),
@@ -223,8 +247,13 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   
+  output$hover_chart <- renderDataTable({
+    hover_df()
+  })
+  
+  # Reactive values ----
   data <- reactive({
     req(input$data_upload)
     
@@ -250,26 +279,54 @@ server <- function(input, output) {
   })
   
   observe({
-    # max_value <- ceiling(log10(max(df %>% select(avg_monthly_searches, jan:dec))))
-    # updateSliderInput(inputId = "growth_range", max = max_value, value = c(-1.5, max_value))
+    # dropdowns
+    keyword_choices <- unique(data()$keyword)
+    grouping_choices <- group_info() %>% 
+      filter(!word %in% tm::stopwords(kind = "SMART")) %>%
+      arrange(desc(n)) %>% pull(word) %>% unique()
+
+    updateSelectInput(inputId = "keyword", choices = keyword_choices)
+    updateSelectInput(inputId = "static_keyword_callout", choices = keyword_choices)
+    updateSelectInput(inputId = "grouping_variable", selected = NULL, choices = grouping_choices)
     
-    choices <- unique(data()$keyword)
-    updateSelectInput(inputId = "keyword", choices = choices)
-    updateSelectInput(inputId = "static_keyword_callout", choices = choices)
-    
-  })
-  
-  observe({
+    # sliders 
     max_yval <- max(c(data()$yo_y_change, data()$three_month_change))
     min_yval <- min(c(data()$yo_y_change, data()$three_month_change))
     range_yval <- max_yval - min_yval
     updateSliderInput(inputId = "growth_range", min = min_yval - range_yval*0.05, max = max_yval + range_yval*0.05,
                       value = c(min_yval - range_yval*0.05, max_yval + range_yval*0.05))
+    
+    # max_xval <- ceiling(log10(max(data() %>% select(avg_monthly_searches, jan:dec))))
+    # updateSliderInput(inputId = "num_posts", max = max_xval, value = c(0.1, max_xval))
+  })
+
+  observeEvent(input$data_upload, {
+    max_searches <- max(data() %>% select(avg_monthly_searches, jan:dec))
+    normaliser <- 10^(floor(log10(abs(max_searches))))
+    adjusted_max_searches <- ceiling(max_searches/normaliser)*normaliser
+    max_xval <- floor(log10(adjusted_max_searches))
+    updateSliderInput(inputId = "num_posts", max = max_xval, value = c(0.1, max_xval))
+    
+    js <- "
+  setTimeout(function() {
+    logifySlider('num_posts', sci = false);
+  }, 5);
+  "
+    session$sendCustomMessage("jsCode", list(code = js))
   })
   
   random_state <- shiny::eventReactive(input$shuffle_label, {
     sample(1:1000, 1)
   }, ignoreNULL = FALSE)
+  
+  group_info <- reactive({
+    data() %>% 
+      select(keyword) %>% 
+      tidytext::unnest_tokens(word, keyword, drop = FALSE) %>% 
+      group_by(word) %>%
+      filter(!word %in% tm::stopwords(kind = "SMART")) %>% 
+      mutate(n=n()) 
+  })
   
   hover_df <- shiny::reactive({
     
@@ -301,13 +358,33 @@ server <- function(input, output) {
                                                 paste0(keyword, " (", avg_monthly_searches, ")", collapse = ", ")))) %>% 
       ungroup() %>% select(group_number, combined_info) %>% distinct()
     
-    joined_df <- plot_df %>%
-      group_by(x,y) %>% mutate(group_number = cur_group_id()) %>%
-      left_join(hover_info, by = "group_number") %>% ungroup() %>%
-      mutate(rowid = row_number())
+    if(length(input$grouping_variable > 0)){
+
+      keywords_to_highlight <- group_info() %>% 
+        filter(word %in% input$grouping_variable) %>% 
+        group_by(keyword) %>%
+        arrange(word) %>%
+        distinct(keyword, word) %>%
+        mutate(group_var = paste0(word, collapse = ", "))
+
+      joined_df <- plot_df %>%
+        group_by(x,y) %>% mutate(group_number = cur_group_id()) %>%
+        left_join(hover_info, by = "group_number") %>% ungroup() %>%
+        left_join(keywords_to_highlight %>% select(keyword, group_var), by = "keyword") %>%
+        # mutate(selected_terms = case_when(keyword %in% keywords_to_highlight ~ input$grouping_variable, TRUE ~ "Other")) %>%
+        mutate(group_var = if_else(is.na(group_var), "Other", group_var)) %>%
+        mutate(rowid = row_number())
+    } else {
+      joined_df <- plot_df %>%
+        group_by(x,y) %>% mutate(group_number = cur_group_id()) %>%
+        left_join(hover_info, by = "group_number") %>% ungroup() %>%
+        mutate(group_var = 1) %>%
+        mutate(rowid = row_number())
+    }
+    
     
     joined_df
-  }) # have removed plot_df and integrated it here
+  }) 
   
   df_highlighted <- shiny::reactive({ 
     plotly::event_data("plotly_selected")
@@ -331,53 +408,57 @@ server <- function(input, output) {
              ax_col = coords()[,1],
              ay_col = coords()[,2])
   })
-
+  
   df_longer <- shiny::reactive({
     hover_df()[hover_df()$rowid %in% df_highlighted()$customdata, ] %>%
       pivot_longer(jan:dec, values_to = "n", names_to = "month") %>%
       mutate(date = as.Date(paste0("01-", stringr::str_to_title(month), "-2022"), format = "%d-%b-%Y")) %>%
       group_by(keyword)
-
+    
   })
   
   basic_scatter <- shiny::reactive({
     
     plot_ly(data = hover_df(), x = ~x, y = ~y,
             type = "scatter", mode = "markers",
-            color = ~log_avg_posts,
+            # color = ~log_avg_posts,
+            color = ~as.factor(group_var),
             customdata = ~rowid,
-            # size = ~log_avg_posts, sizes = c(5, 40),
-            colors = viridis::viridis_pal(option = "A")(nrow(data()))[hover_df()$rowid],
+            size = ~log_avg_posts, sizes = c(5, 30),
+            # colors = viridis::viridis_pal(option = "A")(n_distinct(hover_df()$group_var)),
             hoverinfo = "text",
             text = ~combined_info,
             marker = list(opacity = 0.5,
-                          size = 12,
+                          # size = 12,
                           showscale = FALSE,
                           showlegend = TRUE,
                           sizemode = "diameter",
                           line = list(color = "black",
                                       width = 0.5)
             )) %>%
-      # colorbar(title = list(text = "Log of Average Posts")) %>%
       layout(
         dragmode = "lasso",
         shapes = list(hline(0)),
         margin = list(b=100),
         xaxis = list( type = "log",
-                      title = "Number of Keyword Searches*",
+                      title = ~ifelse(input$xaxis == "avg_monthly_searches", "Average Number of Keyword Searches", 
+                                      paste0(month.name[match(input$xaxis, tolower(month.abb))], " Number of Keyword Searches")),
                       range = c(x_min(), x_max()),
                       zeroline = FALSE, showline = TRUE, mirror = TRUE),
         yaxis = list(zeroline = FALSE, 
                      ticksuffix = "%",
                      showline = TRUE, mirror = TRUE,
                      range = c(input$growth_range),
-                     title = "Growth"),
-        showlegend = FALSE) %>%
-      # group_by(xaxis, change_time) %>%
-      add_annotations(text = "<span style='font-style:italic;'>*Note that this is a logarithmic scale, values 1, 10, 100, 1000 etc. are equally spaced on the graph.\nA log scale means that that keywords with 0 posts will not be displayed as points on the graph</span>",
+                     title = ifelse(input$yaxis == "three_month_change", "Three Month Growth", "Year-on-Year Growth")),
+        showlegend = ~ifelse(all(group_var == 1), FALSE, TRUE),
+        legend = list(title = list(
+          text = "<b>Word(s) in search term:</b>",
+          font = list(size = 12)
+        ) )) %>%
+      add_annotations(text = "<span style='font-style:italic;text-decoration:underline;'>Note:</span><span style='font-style:italic;'> Bubble size corresponds to average monthly searches, a larger bubble represents a larger average monthly search</span>\n<span style='font-style:italic;'>*This is a logarithmic scale, values 1, 10, 100, 1000 etc. are equally spaced on the graph.\nA log scale means that that keywords with 0 posts will not be displayed as points on the graph</span>",
                       # "*",
                       xref = "paper", yref = "paper",
-                      x = 0.5, y = -0.3, size = 3, showarrow = FALSE,
+                      x = 0.5, y = -0.35, size = 3, showarrow = FALSE,
                       font = list(size = 10)) %>%
       plotly::config(
         displaylogo = FALSE,
@@ -404,6 +485,8 @@ server <- function(input, output) {
              "Three Month Growth" = three_month_change, "Year on Year Growth" = yo_y_change,
              !!!setNames(colnames(hover_df())[6:17], month.name))
   })
+  
+  # Outputs ----
   
   output$filter_selected_tab1 <- DT::renderDataTable({
     display_table() %>%
@@ -441,13 +524,13 @@ server <- function(input, output) {
   
   output$vot_plot <- plotly::renderPlotly({
     
-    plot_ly(df_longer(), x = ~date, y = ~n, color = ~keyword,
+    plot_ly(df_longer(), x = ~date, y = ~jitter(n), color = ~keyword,
             type = "scatter", mode = "markers + line",
             hovertemplate = ~paste("<b>", keyword, "</b><br>",
-                                  "Month: %{x|%b}, Searches: %{y}<br><extra></extra>"),
+                                   "Month: %{x|%b}, Searches:", n, "<br><extra></extra>"),
             colors = viridis::viridis_pal(option = "A", end = 0.92)(nrow(df_longer()))) %>%
       layout(xaxis = list(title = "Month"),
-             yaxis = list(title = "Number of Posts"))
+             yaxis = list(title = "Number of Searches"))
   })
   
   output$data_download_tab1 <- shiny::downloadHandler(
@@ -476,6 +559,8 @@ server <- function(input, output) {
       write.csv(display_table(), file)
     }
   )
+  
+  # end ----
   
 }
 
